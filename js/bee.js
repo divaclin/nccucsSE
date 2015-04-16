f6f05e57600e7feab5a2d02c2a6ab79ab94c598c
divac lin <divac.d.zoro@gmail.com>
2015-04-16 14:33:37 +0800

    pos fix

--- js/bee.js
+++ js/bee.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$('iframe').css("margin-top",($(window).height()-480)/2);
+	$('iframe').css("margin-top",($(window).height()-530)/2+50);
 	$('iframe').css("margin-left",($(window).width()-640)/2);
 });
