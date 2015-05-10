631a07dc395a8a2eaf60027d899b848ecad7984e
divac lin <divac.d.zoro@gmail.com>
2015-05-11 02:37:28 +0800

    go

--- js/bee.js
+++ js/bee.js
@@ -8,13 +8,13 @@ $(window).on('hashchange', function() {
 function router(tag){
 	tag=''|tag;
 	tag+=window.location.href.match(/#[a-zA-Z]*/g);
-	console.log(typeof(tag));
+	console.log(tag);
     switch(tag){
 	  case "#about":
 		  $('article').html('<div style="font-size:30px;">this is about</div>');
 		  break;
 	  case "#info":
-		  $('article').html('<div style="font-size:30px;">this is info</div>');
+		  $('article').html('<div></div>');
 		  break;
 	  case "#link":
 		  $('article').html('<div style="font-size:30px;">this is link</div>');
@@ -23,7 +23,7 @@ function router(tag){
 		  $('article').html('<div style="font-size:30px;">this is QA</div>');
 		  break;
 	  default:
-		  $('article').html('<iframe src="FlappyBee/play.html" width="650" height="490"></iframe>');
+		  $('article').html('<iframe src="FlappyBee/play.html"  width="650" height="490"></iframe>');
 	  	  $('iframe').css("margin-top","0px");
 	  	  $('iframe').css("margin-left","155px");
 	  	  $('body').css("overflow","hidden");
