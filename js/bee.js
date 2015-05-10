51a1700cbecb57b94d671719922fc50d8c582db5
divac lin <divac.d.zoro@gmail.com>
2015-05-11 01:54:31 +0800

    test

--- js/bee.js
+++ js/bee.js
@@ -9,6 +9,7 @@ $(window).on('hashchange', function() {
 
 function router(){
 	var tag=window.location.href.match(/#[a-zA-Z]*/g);
+	console.log(tag);
     switch(tag){
 	  case '#about':
 		  $('article').html('<div style="font-size:30px;">this is about</div>');
