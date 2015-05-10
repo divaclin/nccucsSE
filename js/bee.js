37a0159d5cb28af8ace3b3f78258e4a50ef3946c
divac lin <divac.d.zoro@gmail.com>
2015-05-11 00:44:53 +0800

    router 1

--- js/bee.js
+++ js/bee.js
@@ -2,3 +2,7 @@ $(document).ready(function(){
 	$('iframe').css("margin-top",($(window).height()-530)/2+50);
 	$('iframe').css("margin-left",($(window).width()-640)/2);
 });
+$(window).on('hashchange', function() {
+	var tag=window.location.href;
+	console.log(tag);
+});
