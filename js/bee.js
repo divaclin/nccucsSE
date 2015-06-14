fa94e8ef32227f3b9c95ba4eddbdb613f7744c4f
divac lin <divac.d.zoro@gmail.com>
2015-06-15 02:37:45 +0800

    asdasdd

--- js/bee.js
+++ js/bee.js
@@ -19,20 +19,50 @@ Parse.initialize("gkxrXspbCFvMPK3Gn1Vs9Vt9w4FAnLkG9a0v56MA", "yg3rSXgnmwSbYXVfKq
 					  return '<div class="beeContainer"><div class="newInfo"></div></div>';
 				     },
 				link:function(){
-					  return '<div class="beeContainer"></div>';
+					  var RelatedLink = Parse.Object.extend("RelatedLink");
+					  var all; 
+					  $link =$('.link');
+					  
+					  for(var i=1;i<=3;i++){
+					      all = new Parse.Query(RelatedLink); 
+					      all.equalTo("role",i);
+					      all.find({
+						      success:function(data){
+								  switch(i){
+								     case 1:
+										 $link.append('<h3>推廣網站</h3>');
+										 $link.appen('<a href="'+data[i].get('url')+'"><h4>'+data[i].get('name')+'</h4></a>');
+										 $link.append('<img style="width:170px; height:60px;" src="'+data[i].get('img').url()+'" />');
+										 break;
+								     case 2:
+										 $link.append('<h3>知名業者</h3>');
+										 $link.appen('<a href="'+data[i].get('url')+'"><h4>'+data[i].get('name')+'</h4></a>');
+										 $link.append('<img style="width:170px; height:60px;" src="'+data[i].get('img').url()+'" />');	 
+										 break;
+								     case 3:
+										 $link.append('<h3>新聞報導</h3>');
+										 $link.appen('<a href="'+data[i].get('url')+'"><h4>'+data[i].get('name')+'</h4></a>');
+										 break;
+								     default:
+										 break; 	
+								  }
+						      },
+						      error:function(error){
+                                 console.log(error) ;
+                                 alert(error.message);
+						      }
+					     });
+				      }
+					  return '<div class="beeContainer"><div class="link"></div></div>';
 				     },
 				QA:function(){
 				    var CommonQuestion = Parse.Object.extend("CommonQuestion");
 				    var all = new Parse.Query(CommonQuestion);
+					$QA =  $(".QA");
 					all.find({
 						success:function(data){
-							// var t = setTimeout(function(){
-							// 	  for(var i=0;i<data.length;i++){
-							// 		  $(".QA").append('<h3>Q'+(i+1)+'. '+data[i].get('Question')+'</h3><p>'+data[i].get('Answer')+'</p>');
-							// 	   }
-							// 	 },50);
 						  for(var i=0;i<data.length;i++){
-							  $(".QA").append('<h3>Q'+(i+1)+'. '+data[i].get('Question')+'</h3><p>'+data[i].get('Answer')+'</p>');
+							  $QA.append('<h3>Q'+(i+1)+'. '+data[i].get('Question')+'</h3><p>'+data[i].get('Answer')+'</p>');
 						   }
 						},
 						error: function(error) {
