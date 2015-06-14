61f63d0758100a03ded4da31af80b3e87fac6da5
divac lin <divac.d.zoro@gmail.com>
2015-06-14 23:45:56 +0800

    asdasdad

--- js/bee.js
+++ js/bee.js
@@ -8,16 +8,16 @@
 				
 		Router.prototype.page = function(){
 			var tag = window.location.href.match(/#[a-zA-Z]*/g);
+			tag = tag.substr(1);
 			console.log(tag);
 			var text = '';
 			var switchView = {
-				#about:'<div class="beeContainer"><p>about</p></div>',
-				#info:'<div class="beeContainer"><div class="newInfo"></div></div>',
-				#linkL:'<div class="beeContainer"></div>',
-				#QA:'<div class="beeContainer"><p>QA</p></div>',
-				#legalInfo:'',
-				#beeInfo:'',
-				#:''
+				about:'<div class="beeContainer"><p>about</p></div>',
+				info:'<div class="beeContainer"><div class="newInfo"></div></div>',
+				linkL:'<div class="beeContainer"></div>',
+				QA:'<div class="beeContainer"><p>QA</p></div>',
+				legalInfo:'',
+				beeInfo:'',
 			};
 			text = (switchView.indexOf(tag)==-1'<iframe style="margin-top:0px; margin-left:155px;" src="FlappyBee/play.html"  width="650" height="490"></iframe>'?:switchView[tag]);
 			// 		    switch(tag){
