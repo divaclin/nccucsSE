f319c2286641edb83c4f10690c440b454918ccb0
divac lin <divac.d.zoro@gmail.com>
2015-06-22 02:30:32 +0800

    sadasdd

--- js/bee.js
+++ js/bee.js
@@ -116,7 +116,5 @@ $(document).on('mouseout','#secondUl',function(e){
 $(document).on('click','.dotCircle',function(e){
 	 var start = $("a[data-num=0]").offset().top;
      var target = $("a[data-num="+$(this).attr('data-num')+"]");
-  	$('.QA').animate({scrollTop: target.offset().top-start},'slow');
-	
-   // $('.QA').animate({scrollTop: 535*parseInt($(this).attr('data-num'))},'slow');
+  	 $('.QA').animate({scrollTop: target.offset().top-start},'slow');
 });
