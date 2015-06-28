ec0e20c3d70011f25bcfc32b8bb220bafd653faf
divac lin <divac.d.zoro@gmail.com>
2015-06-29 03:13:00 +0800

    sdasdd

--- js/bee.js
+++ js/bee.js
@@ -42,7 +42,7 @@ Parse.initialize("gkxrXspbCFvMPK3Gn1Vs9Vt9w4FAnLkG9a0v56MA", "yg3rSXgnmwSbYXVfKq
 						success:function(data){
  						  var text = '<div class="dotSelector">';	
 						  for(var i=0;i<data.length;i++){
-							  $(".QA").append('<a data-num="'+i+'"></a><div class="QAbox"><h3>Q'+(i+1)+'. '+data[i].get('Question')+'</h3><pre id="pre'+i+'" style="max-height:180px; overflow:scroll;">'+data[i].get('Answer')+'</pre></div>');
+							  $(".QA").append('<a data-num="'+i+'"></a><div class="QAbox"><h3>Q'+(i+1)+'. '+data[i].get('Question')+'</h3><pre id="pre'+i+'" style="overflow:scroll;">'+data[i].get('Answer')+'</pre></div>');
 							  console.log(data[i].get('csv').url());
 							  drawPie(data[i].get('csv').url(),i);
 							  text+='<div class="dotCircle" data-num="'+i+'"></div>';
